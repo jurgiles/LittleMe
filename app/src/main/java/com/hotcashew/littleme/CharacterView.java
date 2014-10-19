9f0a746da6a3a46416fd9ca2b7b4ee3bd229b434
Alex Barkan <alex@augmate.com>
2014-10-19 12:49:10 -0400

    added running animation loop

--- app/src/main/java/com/hotcashew/littleme/CharacterView.java
+++ app/src/main/java/com/hotcashew/littleme/CharacterView.java
@@ -9,13 +9,19 @@ import android.graphics.Rect;
 import android.graphics.RectF;
 import android.os.SystemClock;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class CharacterView extends View {
-
     private Bitmap runningSprite; // running loop spritesheet
     private Rect spriteDimensions;
     private long lastFrameTimestamp = 0;
+
+    // running animation loop properties
+    private int runDurationMsec = 1000;
+    private int runProgressMsec = 0;
+    private int runTotalFrames = 8;
+    private int runCurrentFrameIdx = 0;
 
     public CharacterView(Context context) {
         super(context);
@@ -39,7 +45,7 @@ public class CharacterView extends View {
 
     // tick forward the animation
     void update() {
-        long now = SystemClock.currentThreadTimeMillis();
+        long now = SystemClock.elapsedRealtime();
         long frameDuration = now - lastFrameTimestamp;
         lastFrameTimestamp = now;
 
@@ -47,19 +53,22 @@ public class CharacterView extends View {
         if (frameDuration > 1000)
             return;
 
-        // TODO: advance to next frame using time
+        runProgressMsec = (runProgressMsec + (int) frameDuration) % runDurationMsec;
+        runCurrentFrameIdx = (int) Math.floor((float) runTotalFrames * (float) runProgressMsec / (float) runDurationMsec);
+//        Log.d("CharacterView", String.format("frame idx: %d (frame duration=%d msec | total=%d msec)", runCurrentFrameIdx, frameDuration, runProgressMsec));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        update();
 
-        float scale = 0.5f;
-        Rect spriteSource = new Rect(0, 0, spriteDimensions.width(), spriteDimensions.height());
-        RectF screenDestination = new RectF(0, 0, spriteDimensions.width() * scale, spriteDimensions.height() * scale);
+        float spriteDstScale = 0.5f;
+        Rect spriteSrc = new Rect(spriteDimensions.width() * runCurrentFrameIdx, 0, spriteDimensions.width() * runCurrentFrameIdx + spriteDimensions.width(), spriteDimensions.height());
+        RectF spriteDst = new RectF(0, 0, spriteDimensions.width() * spriteDstScale, spriteDimensions.height() * spriteDstScale);
 
         canvas.drawColor(Color.WHITE); // clear screen
-        canvas.drawBitmap(runningSprite, spriteSource, screenDestination, null); // draw sprite
+        canvas.drawBitmap(runningSprite, spriteSrc, spriteDst, null); // draw sprite
 
         postInvalidate();
     }
